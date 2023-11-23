9e0bc2fe1a046885f6f894e69b7c849ad6a5c850
JonnathanMS <jhonnnario@gmail.com>
2023-11-23 00:11:05 -0500

    connectTimeoutMS

--- src/db.js
+++ src/db.js
@@ -7,7 +7,7 @@ export const connectDB = async () => {
     try {
         // await mongoose.connect(`mongodb+srv://jonnathanmonroybd:${TOKEN_SECRET}@cluster0.j8kwwty.mongodb.net/?retryWrites=true&w=majority`);
         const url = process.env.MONGODB_CONNECT_URI;
-        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, connectTimeoutMS: 30000 })
         console.log("MongoDB is connected");
         // router.get("/", (req, res) => {
         //     res.send("Esta activo el backend")
