31157ab0cc346cbd3593afb58cb4155675cc608d
JonnathanMS <jhonnnario@gmail.com>
2023-11-24 21:03:26 -0500

    Agrego mensaje de informacion para el api

--- src/app.js
+++ src/app.js
@@ -28,7 +28,61 @@ app.use("/api", authRoutes);// asi se modifica la url// app.use(authRoutes); asi
 app.use("/api", taskRoutes);
 app.use("/api", citasGeneralRoutes);
 app.use("/", (req, res) => {
-    res.send("En este momento Jonnathan Monroy esta trabajando en esta Api. Pronto estará lista. Para cualquier consulta ponte en contacto al correo jhonnnario@gmai.com");
+    res.send(`Api desarrolada por Jonnathan Monroy,Gracias por estar aquí! 
+
+   Si quieres ver datos ya creados y crear nuevos puedes acceder con esteusuario de
+   pruebas (si lo prefieres crea uno nuevo, pero este ya tiene datos para consultar):
+   {
+  "email":"jhonnnario@gmail.com",
+  "password":"123456"
+   }
+   
+   (Endpoint):
+   https://nuevobackend.onrender.com/api/login  
+   https://nuevobackend.onrender.com/api/register 
+   https://nuevobackend.onrender.com/api/citasGeneral
+   https://nuevobackend.onrender.com/api/tasks
+   https://nuevobackend.onrender.com/api/logout
+
+    Si quieres probar esta api puedes usar lo siguiente en el query body json
+
+    Registrarse:
+
+    {
+  "username": "nombre de usuario",
+  "nombres": "nombre",
+  "apellidos": "apellido",
+  "cedula": "numero",
+  "celular": "numero",
+  "citas": [],
+  "fecha": "2023-01-07T00:00:00.000Z",
+  "email": "email@gmail.com",
+  "password": "password"
+}
+
+loguearse:
+{
+  "email":"email@gmail.com",
+  "password":"password"
+}
+
+Generar nueva cita tutoria:
+{
+    "tema": "tema",
+    "tutor": "username",
+    "descripcion": "descripcion",
+    "hora": "16:05",
+    "fecha": "2023-11-25T00:00:00.000Z"
+}
+
+Generar nueva tarea:
+{
+  "title": "tarea1",
+  "description": "tarea",
+  "date": "2023-11-25T20:54:25.935Z"
+}
+
+`);
 })
 
 export default app;
